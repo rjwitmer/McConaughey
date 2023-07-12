b398557b7a78d2032ff4d89bc942e243e3e230c0
Bob Witmer <bwitmer@golden.net>
2023-07-12 16:47:32 -0400

    Initial Commit

---
+++ McConaughey/McConaugheyApp.swift
@@ -0,0 +1,17 @@
+//
+//  McConaugheyApp.swift
+//  McConaughey
+//
+//  Created by Bob Witmer on 2023-07-12.
+//
+
+import SwiftUI
+
+@main
+struct McConaugheyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
