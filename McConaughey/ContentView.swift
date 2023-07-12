2735cc1c7b85117abd90340ed59ee9d397ee30c9
Bob Witmer <bwitmer@golden.net>
2023-07-12 18:05:56 -0400

    Week 5

--- McConaughey/ContentView.swift
+++ McConaughey/ContentView.swift
@@ -6,21 +6,82 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    let titleMessage = "How is Matt?"
+    let alrightMessage = "How Alright is Matt?"
+    @State private var alrightValue = 3
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(titleMessage)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            Image("matt")
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 10)
+                .padding(
+                )
+            
+            HStack {
+                //                Text(alrightMessage)
+                //                    .font(.title2)
+                
+
+            Stepper(alrightMessage, value: $alrightValue, in: 0...10)
+                    .font(.title2)
+                
+                
+            }
+            .padding()
+            
+            Text("\(alrightValue)")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            Button("Tell Me!") {
+                playSound(soundName: "alright")
+            }
+            .buttonStyle(.bordered)
+            .bold()
+            .accentColor(.accentColor)
+
         }
         .padding()
+        
+
+    }
+    
+    // Function to Play a sound
+    // Requires:    import AVFAudio
+    //              @State private var audioPlayer: AVAudioPlayer!
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName).")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            if alrightValue != 0 {
+                audioPlayer.numberOfLoops = alrightValue - 1
+                audioPlayer.play()
+            }
+
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer.")
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
